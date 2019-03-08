68f7c14c364bad1a7cb80d6a26d5abf6894131a6
Maddog0057 <maddog0057@gmail.com>
2019-03-08 00:07:36 -0500

    logging fix

--- zettabyte.py
+++ zettabyte.py
@@ -18,6 +18,38 @@ hours = 6
 api = ns.Nationstates(useragent)
 nation = api.nation(nationName, autologin=password)
 
+logDir = config["system"]["log"]
+logFile = logDir+config["discord"]["name"]+".log"
+
+
+class StreamToLogger(object):
+
+   def __init__(self, logger, log_level=logging.INFO):
+      self.logger = logger
+      self.log_level = log_level
+      self.linebuf = ''
+
+   def write(self, buf):
+      for line in buf.rstrip().splitlines():
+         self.logger.log(self.log_level, line.rstrip())
+
+handler = RotatingFileHandler(logFile,"a",maxBytes=1048576,backupCount=5)
+
+logging.basicConfig(
+   level=logging.INFO,
+   format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+   handlers = [ handler ]
+)
+
+
+stdout_logger = logging.getLogger('STDOUT')
+sl = StreamToLogger(stdout_logger, logging.INFO)
+sys.stdout = sl
+
+stderr_logger = logging.getLogger('STDERR')
+sl = StreamToLogger(stderr_logger, logging.ERROR)
+sys.stderr = sl
+
 def get_pin():
 	headers = {'user-agent': useragent, 'X-Autologin': password}
 	purl = "https://www.nationstates.net/cgi-bin/api.cgi?nation="+nationName+"&q=unread"
